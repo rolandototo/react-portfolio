d30f31db74cb721c24cec41095514dab6a8b21b0
Rolando Escobar <rolandototo@gmail.com>
2022-05-21 15:34:17 -0600

    Modificacion e integracion de i18 en index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import {I18nextProvider} from "react-i18next"
+import i18next from "i18next"
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+i18next.init({
+    interpolation: { escapeValue: false },
+})
+
+ReactDOM.render(
+
+<I18nextProvider i18={i18next}>
+<App/>
+</I18nextProvider>
+
+,
+ document.querySelector("#root")
+ )
